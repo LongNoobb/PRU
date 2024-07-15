4b8d5c58c5ef6c602744d07bc43d30b5529427f2
Nguyễn Lê Đức Hiếu <hieunld08.work@gmail.com>
2024-07-15 23:19:07 +0700

    update boss

---
+++ Assets/Script/Enemy/BossHealth.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealth : MonoBehaviour
+{
+    public float currentHealth;
+    public float maxHealth;
+    public GameObject healthBarUI;
+    public Slider slider;
+
+    void Start()
+    {
+        currentHealth = maxHealth * 2; // Double the max health for the boss
+        slider.value = CalculateHealth();
+        healthBarUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        slider.value = CalculateHealth();
+
+        if (currentHealth < maxHealth * 2)
+        {
+            healthBarUI.SetActive(true);
+        }
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (currentHealth > maxHealth * 2)
+        {
+            currentHealth = maxHealth * 2;
+        }
+    }
+
+    float CalculateHealth()
+    {
+        return currentHealth / (maxHealth * 2);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            gameObject.SetActive(false);
+        }
+
+        slider.value = CalculateHealth();
+    }
+}
---
+++ Assets/Script/Enemy/BossMove.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+
+public class BossMove : MonoBehaviour
+{
+    public float moveSpeed;
+    public float nextWPDistance;
+    public bool roaming = true;
+    public Seeker seeker;
+    bool reachDestination = false;
+    public bool updateContinuesPath;
+
+    public bool isShootable = false;
+    public GameObject bullet;
+    public float bulletSpeed;
+    public float timeBtwFire;
+    private float fireCooldown;
+    Path path;
+    Coroutine moveCoroutine;
+    private bool playerInRoom = false;
+
+    private BossHealth bossHealth;
+
+    void Start()
+    {
+        bossHealth = gameObject.GetComponent<BossHealth>();
+        reachDestination = true;
+        InvokeRepeating("CalculatePath", 0f, 0.5f);
+    }
+
+    private void Update()
+    {
+        if (!playerInRoom) return;
+
+        fireCooldown -= Time.deltaTime;
+        if (fireCooldown < 0)
+        {
+            fireCooldown = timeBtwFire;
+            BossFireBullets();
+        }
+    }
+
+    void BossFireBullets()
+    {
+        Vector3[] directions = new Vector3[]
+        {
+            Vector3.right,
+            Vector3.left,
+            Vector3.up,
+            Vector3.down,
+            (Vector3.right + Vector3.up).normalized,
+            (Vector3.right + Vector3.down).normalized,
+            (Vector3.left + Vector3.up).normalized,
+            (Vector3.left + Vector3.down).normalized
+        };
+
+        foreach (var dir in directions)
+        {
+            var bulletTmp = Instantiate(bullet, transform.position, Quaternion.identity);
+            Rigidbody2D rb = bulletTmp.GetComponent<Rigidbody2D>();
+            rb.AddForce(dir * bulletSpeed, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "bullet")
+        {
+            bossHealth.TakeDamage(1);
+        }
+    }
+
+    void CalculatePath()
+    {
+        if (!playerInRoom) return;
+
+        Vector2 target = FindTarget();
+        if (seeker.IsDone() && (reachDestination || updateContinuesPath))
+            seeker.StartPath(transform.position, target, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (p.error) return;
+        path = p;
+        MoveToTarget();
+    }
+
+    void MoveToTarget()
+    {
+        if (moveCoroutine != null) StopCoroutine(moveCoroutine);
+        if (gameObject.activeInHierarchy)
+            moveCoroutine = StartCoroutine(MoveToTargetCoroutine());
+    }
+
+    IEnumerator MoveToTargetCoroutine()
+    {
+        int currentWP = 0;
+        reachDestination = false;
+        while (currentWP < path.vectorPath.Count)
+        {
+            Vector2 direction = ((Vector2)path.vectorPath[currentWP] - (Vector2)transform.position).normalized;
+            Vector2 force = direction * moveSpeed * Time.deltaTime;
+            transform.position += (Vector3)force;
+
+            float distance = Vector2.Distance(transform.position, path.vectorPath[currentWP]);
+            if (distance < nextWPDistance)
+            {
+                currentWP++;
+            }
+
+            yield return null;
+        }
+
+        reachDestination = true;
+    }
+
+    Vector2 FindTarget()
+    {
+        Vector3 playerPos = FindObjectOfType<MoveController>().transform.position;
+        if (roaming == true)
+        {
+            return (Vector2)playerPos + (Random.Range(10f, 50f) * new Vector2(Random.Range(-1, 1), Random.Range(-1, 1)).normalized);
+        }
+        else
+        {
+            return playerPos;
+        }
+    }
+
+    public void PlayerEnteredRoom()
+    {
+        Debug.Log("Boss detected player entering the room");
+        playerInRoom = true;
+        CalculatePath();
+    }
+}
